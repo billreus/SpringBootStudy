bfefa1dae8b1c5d778bb0b4caba0593f864b9c09
billreus <rine328909144@gmail.com>
2019-07-08 18:00:25 +0800

    building

---
+++ ssm-demo/src/test/java/com/demo/BaseTest.java
@@ -0,0 +1,4 @@
+package java.com.demo;
+
+public class BaseTest {
+}
---
+++ ssm-demo/target/classes/db.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
